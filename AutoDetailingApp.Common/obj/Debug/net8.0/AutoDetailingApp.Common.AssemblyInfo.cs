7cc5e800fd147967b2c168fbd2d7363dab349aca
Anton Kenderov <shikagamingbg@gmail.com>
2025-04-24 00:09:11 +0300

    Added index page, css for it and js for the navigation bar. Also made the layout page.

--- AutoDetailingApp.Common/obj/Debug/net8.0/AutoDetailingApp.Common.AssemblyInfo.cs
+++ AutoDetailingApp.Common/obj/Debug/net8.0/AutoDetailingApp.Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoDetailingApp.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c5d7126f5293208cceef58a35da32388b0bc049")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoDetailingApp.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoDetailingApp.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ AutoDetailingApp/wwwroot/js/navigation.js
@@ -0,0 +1,34 @@
+﻿function initSmartNavbar() {
+    const navbar = document.getElementById('mainNavbar');
+    if (!navbar) return;
+
+    let lastScrollTop = 0;
+    const navbarHeight = navbar.offsetHeight;
+
+    const mainContent = document.querySelector('.main');
+    if (mainContent) {
+        mainContent.style.marginTop = `${navbarHeight}px`;
+    }
+
+    function handleScroll() {
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (scrollTop > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+
+        if (scrollTop > lastScrollTop && scrollTop > navbarHeight) {
+            navbar.classList.add('hidden');
+        } else {
+            navbar.classList.remove('hidden');
+        }
+
+        lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+    }
+
+    window.addEventListener('scroll', handleScroll);
+}
+
+document.addEventListener('DOMContentLoaded', initSmartNavbar);
