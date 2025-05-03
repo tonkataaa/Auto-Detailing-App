ba5111605826b922c3fdeb2fce145cfc65e524ec
Anton Kenderov <shikagamingbg@gmail.com>
2025-05-03 13:29:41 +0300

    Added Controller for ,,Reservation'' and fixed the layout.

---
+++ AutoDetailingApp/Controllers/ReservationController.cs
@@ -0,0 +1,21 @@
+﻿using AutoDetailingApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoDetailingApp.Controllers
+{
+    public class ReservationController : Controller
+    {
+        private readonly AutoDetailingDbContext dbContext;
+
+		public ReservationController(AutoDetailingDbContext dbContext)
+		{
+            this.dbContext = dbContext;
+		}
+
+		[HttpGet]
+        public async Task<IActionResult> Reservation()
+        {
+            return this.View();
+        }
+    }
+}
