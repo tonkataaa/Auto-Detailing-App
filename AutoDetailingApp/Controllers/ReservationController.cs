2a2064b3ac4db1cf1072806cd3b3712c4ba2b519
Anton Kenderov <shikagamingbg@gmail.com>
2025-05-09 00:29:12 +0300

    Added ReservationForm method.

--- AutoDetailingApp/Controllers/ReservationController.cs
+++ AutoDetailingApp/Controllers/ReservationController.cs
@@ -1,5 +1,12 @@
 ﻿using AutoDetailingApp.Data;
+using AutoDetailingApp.Models;
+using AutoDetailingApp.Web.ViewModels.Reservation;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace AutoDetailingApp.Controllers
 {
@@ -15,7 +22,63 @@ namespace AutoDetailingApp.Controllers
 		[HttpGet]
         public async Task<IActionResult> Reservation()
         {
-            return this.View();
+            var servicesFromDb = await dbContext.Services.ToListAsync();
+
+			var serviceItems = servicesFromDb
+	            .Select(s => new SelectListItem
+	            {
+		            Value = s.Id.ToString(),
+		            Text = s.Name
+	            })
+	            .ToList();
+
+			var model = new ReservationFormModel
+            {
+                AvailableServices = serviceItems,
+                DateForReservation = DateTime.Now
+            };
+
+            return this.View(model);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> ReservationForm(ReservationFormModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.View("Reservation");
+            }
+
+            var selectedSerivce = await dbContext.Services
+                .FirstOrDefaultAsync(s => s.Id == model.ServiceId);
+
+			if (selectedSerivce == null)
+			{
+				ModelState.AddModelError("", "Избраната услуга не е валидна");
+				return View("Reservation", model);
+			}
+
+            Guid userId = Guid.Parse(User.Identity.GetUserId());
+            
+
+			var appointment = new Appointment
+            {
+                FullName = model.FullName,
+                PhoneNumber = model.PhoneNumber,
+                Email = model.Email,
+                DateTime = model.DateForReservation,
+                ServiceId = model.ServiceId,
+                Comment = model.Comment,
+                Status = "Pending",
+                CreatedAt = DateTime.UtcNow,
+                UserId = userId 
+            };
+
+            dbContext.Appointments.Add(appointment);
+            await dbContext.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Reservation));
         }
     }
 }
