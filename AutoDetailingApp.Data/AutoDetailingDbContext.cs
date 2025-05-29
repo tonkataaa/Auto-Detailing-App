f7f8d3fb1dbd95c7ed77f46cefc4879880007f16
Anton Kenderov <shikagamingbg@gmail.com>
2025-05-29 23:18:52 +0300

    Added ADMIN role.

--- AutoDetailingApp.Data/AutoDetailingDbContext.cs
+++ AutoDetailingApp.Data/AutoDetailingDbContext.cs
@@ -7,18 +7,22 @@ namespace AutoDetailingApp.Data
 
 	using AutoDetailingApp.Models;
 	using System.Reflection;
+	using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+	using Microsoft.Extensions.Configuration;
 
 	public class AutoDetailingDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
 	{
+		private readonly IConfiguration _configuration;
+
 		public AutoDetailingDbContext()
 		{
 			
 		}
 
-		public AutoDetailingDbContext(DbContextOptions options)
+		public AutoDetailingDbContext(DbContextOptions options, IConfiguration configuration)
 			: base(options)
 		{
-			
+			_configuration = configuration;
 		}
 
 		public virtual DbSet<Appointment> Appointments { get; set; } = null!;
@@ -29,8 +33,26 @@ namespace AutoDetailingApp.Data
 
 		public virtual DbSet<Service> Services { get; set; } = null!;
 
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{	
+			if (!optionsBuilder.IsConfigured)
+			{
+				optionsBuilder.UseSqlServer(
+					_configuration.GetConnectionString("AutoDetailingDb"),
+					b => b.MigrationsAssembly("AutoDetailingApp.Data"));
+			}
+		}
+
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
+			modelBuilder.Entity<Service>(e =>
+			{
+				e.Property(ent => ent.DurationMinutes)
+				.HasColumnType("bigint")
+				.HasConversion(new TimeSpanToTicksConverter());
+			});
+
+			
 			base.OnModelCreating(modelBuilder);
 			modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 		}
--- AutoDetailingApp.Data/Seeding/DbSeeder.cs
+++ AutoDetailingApp.Data/Seeding/DbSeeder.cs
@@ -1,6 +1,46 @@
 ﻿namespace AutoDetailingApp.Data.Seeding
 {
+	using AutoDetailingApp.Models;
+	using Microsoft.AspNetCore.Identity;
+	using Microsoft.Extensions.Configuration;
+	using Microsoft.Extensions.DependencyInjection;
+	using Microsoft.Extensions.Options;
+
 	public class DbSeeder
 	{
+		public static async Task CreateAdminWithRoleAsync(IServiceProvider serviceProvider)
+		{
+			var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
+			var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+			var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+
+			var adminEmail = configuration["Admin:Email"];
+			var adminPassword = configuration["Admin:Password"];
+
+			var roleExist = await roleManager.RoleExistsAsync("Admin");
+			var userExist = await userManager.FindByEmailAsync(adminEmail);
+
+			if (!roleExist)
+			{
+				await roleManager.CreateAsync(new IdentityRole<Guid>("Admin"));
+			}
+
+			if (userExist == null)
+			{
+				var user = new ApplicationUser
+				{
+					UserName = adminEmail,
+					Email = adminEmail,
+					PasswordHash = adminPassword
+				};
+
+				var result = await userManager.CreateAsync(user, "admin");
+				if (result.Succeeded)
+				{
+					await userManager.AddToRoleAsync(user, "Admin");
+				}
+			}
+		}
 	}
+
 }
