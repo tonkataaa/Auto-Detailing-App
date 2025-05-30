798985d50a3aed2e3295cbbda8c4f03069a6812d
Anton Kenderov <shikagamingbg@gmail.com>
2025-05-30 14:12:46 +0300

    Added configuration method in Program.cs

--- AutoDetailingApp/Program.cs
+++ AutoDetailingApp/Program.cs
@@ -5,29 +5,34 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using AutoDetailingApp.Data;
 using AutoDetailingApp.Models;
-using System.Reflection;
 using AutoDetailingApp.Data.Repository.Interfaces;
 using AutoDetailingApp.Data.Repository;
 using AutoDetailingApp.Services.Data.Interfaces;
 using AutoDetailingApp.Services.Data;
+using AutoDetailingApp.Data.Seeding;
 
 
 var builder = WebApplication.CreateBuilder(args);
-
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var config = new ConfigurationBuilder()
+	.AddJsonFile($"appsettings.json", true, true)
+	.AddEnvironmentVariables()
+	.AddUserSecrets<Program>()
+	.Build();
+
+
+
 builder.Services.AddDbContext<AutoDetailingDbContext>(options =>
 	options.UseSqlServer(connectionString));
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+var identityConfig = builder.Configuration.GetSection("IdentityOptions");
 builder.Services.AddIdentity<ApplicationUser, IdentityRole<Guid>>(options =>
 {
-	options.Password.RequireDigit = true;
-	options.Password.RequiredLength = 12;               
-	options.Password.RequireNonAlphanumeric = true;    
-	options.Password.RequireUppercase = true;          
-	options.Password.RequireLowercase = true;          
+	ConfigureIdentity(builder, options);        
 })
+.AddRoles<IdentityRole<Guid>>()
 .AddEntityFrameworkStores<AutoDetailingDbContext>()
 .AddDefaultUI()
 .AddSignInManager<SignInManager<ApplicationUser>>()
@@ -42,13 +47,6 @@ builder.Services.AddScoped<IReservationService, ReservationService>();
 
 var app = builder.Build();
 
-app.UseRequestLocalization(new RequestLocalizationOptions
-{
-	DefaultRequestCulture = new RequestCulture("bg-BG"),
-	SupportedCultures = new[] { new CultureInfo("bg-BG") },
-	SupportedUICultures = new[] { new CultureInfo("bg-BG") }
-});
-
 if (app.Environment.IsDevelopment())
 {
 	app.UseMigrationsEndPoint();
@@ -59,6 +57,7 @@ else
 	app.UseHsts();
 }
 
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
@@ -66,9 +65,43 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
+//Adding Admin
+using (var scope = app.Services.CreateScope())
+{
+	var services = scope.ServiceProvider;
+	await DbSeeder.CreateAdminWithRoleAsync(services);
+}
+
+app.MapControllerRoute(
+	name: "admin",
+	pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 app.MapControllerRoute(
 	name: "default",
 	pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
+
+static void ConfigureIdentity(WebApplicationBuilder builder, IdentityOptions cfg)
+{
+	cfg.Password.RequireDigit =
+		builder.Configuration.GetValue<bool>("IdentityOptions:Password:RequireDigits");
+	cfg.Password.RequiredLength =
+		builder.Configuration.GetValue<int>("IdentityOptions:Password:RequireLength");
+	cfg.Password.RequireNonAlphanumeric =
+		builder.Configuration.GetValue<bool>("IdentityOptions:Password:RequireNonAlphanumeric");
+	cfg.Password.RequireUppercase =
+		builder.Configuration.GetValue<bool>("IdentityOptions:Password:RequireUppercase");
+	cfg.Password.RequireLowercase =
+		builder.Configuration.GetValue<bool>("IdentityOptions:Password:RequireLowercase");
+
+	cfg.SignIn.RequireConfirmedAccount =
+		builder.Configuration.GetValue<bool>("Identity:SignIn:RequireConfirmedAccount");
+	cfg.SignIn.RequireConfirmedEmail =
+		builder.Configuration.GetValue<bool>("Identity:SignIn:RequireConfirmedEmail");
+	cfg.SignIn.RequireConfirmedPhoneNumber =
+		builder.Configuration.GetValue<bool>("Identity:SignIn:RequireConfirmedPhoneNumber");
+
+	cfg.User.RequireUniqueEmail =
+		builder.Configuration.GetValue<bool>("IdentityOptions:User:RequireUniqueEmail");
+}
